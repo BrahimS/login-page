a5b6107c94c70a649c768d458e4d929dcc3efffa
BS <bs@bss-mac-pro.home>
2020-03-17 16:59:37 +0100

    initialize the firebase API

---
+++ login-firebase/frontend/src/main.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import firebase from 'firebase';
+
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAv57XfPe7W_mHCZcZb8YhVz3ou8243bQ",
+  authDomain: "login-641b9.firebaseapp.com",
+  databaseURL: "https://login-641b9.firebaseio.com",
+  projectId: "login-641b9",
+  storageBucket: "login-641b9.appspot.com",
+  messagingSenderId: "31843875043",
+  appId: "1:31843875043:web:5e7f4c8662b980a0bbadd6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
